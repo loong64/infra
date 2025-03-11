2e1ca1d321806d0e80b9da8c5e819774300d065c
吴小白 <296015668@qq.com>
2025-03-11 18:18:31 +0800

    feat: openssl 3.4.1
    Signed-off-by: 吴小白 <296015668@qq.com>

--- cryptography-linux/openssl-version.sh
+++ cryptography-linux/openssl-version.sh
@@ -1,3 +1,3 @@
-export OPENSSL_VERSION="openssl-3.4.0"
-export OPENSSL_SHA256="e15dda82fe2fe8139dc2ac21a36d4ca01d5313c75f99f46c4e8a27709b7294bf"
+export OPENSSL_VERSION="openssl-3.4.1"
+export OPENSSL_SHA256="002a2d6b30b58bf4bea46c43bdd96365aaf8daa6c428782aa4feee06da197df3"
 export OPENSSL_BUILD_FLAGS="no-ssl3 no-ssl3-method no-zlib no-shared no-module no-comp no-dynamic-engine no-apps no-docs no-sm2-precomp no-atexit"
