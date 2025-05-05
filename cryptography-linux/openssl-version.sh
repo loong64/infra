0049d39edda2c766dc2a42918e6bd21ef35c56c2
吴小白 <296015668@qq.com>
2025-05-05 10:32:36 +0800

    perf: 使用补丁修补

--- cryptography-linux/openssl-version.sh
+++
@@ -1,7 +0,0 @@
-export OPENSSL_VERSION="openssl-3.4.1"
-export OPENSSL_SHA256="002a2d6b30b58bf4bea46c43bdd96365aaf8daa6c428782aa4feee06da197df3"
-# We need a base set of flags because on Windows using MSVC
-# enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
-export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-module no-comp no-dynamic-engine no-apps no-docs no-sm2-precomp no-atexit"
-export OPENSSL_BUILD_FLAGS_ARMV7L="linux-armv4 ${OPENSSL_BUILD_FLAGS_WINDOWS}"
-export OPENSSL_BUILD_FLAGS="${OPENSSL_BUILD_FLAGS_WINDOWS} enable-ec_nistp_64_gcc_128"
